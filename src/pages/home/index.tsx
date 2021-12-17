e731dd879311d16451f86d86cb4a54d310491155
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-12-17 12:57:19 -0300

    feat: ensure axios return values

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../components/header'
+import { api } from '../../services/api'
+// import { Trips } from '../../utils'
 
 export default function Home () {
+  // const [reserves, setReserves] = React.useState<Trips[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('/trips')
+      console.log(response.data)
+    })()
+  }, [])
+
   return (
     <div>
       <Header />
---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export type Trips = {
+  id: number;
+  title: string;
+  status: boolean;
+  image: string
+
+}
